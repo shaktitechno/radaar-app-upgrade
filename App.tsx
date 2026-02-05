e617d2beea38bef282cd8c2b8491f13fa196b9dc
shaktitechno <shakti.kumar@technocratshorizons.com>
2026-02-05 18:54:19 +0530

    splash screen completed successfully

--- App.tsx
+++ App.tsx
@@ -4,44 +4,97 @@
  *
  * @format
  */
-import 'react-native-reanimated'; 
+import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import StackNavigation from './src/navigation/stackNavigation';
-// import { useRecoilState } from 'recoil';
-import {  useAuthToken } from './src/recoil/atoms/authToken';
-import { useState } from 'react';
+import { useAuthToken } from './src/recoil/atoms/authToken';
+import { useCallback, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  getProfile,
+  getToken,
+  initializeSocket,
+  socket,
+} from './src/services/apiConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as RootNavigation from './src/services/navigationServices';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
- const token = useAuthToken(state => state.authToken);
-   const [intro,setIntro] = useState<boolean | null>(null)
+  const token = useAuthToken(state => state.authToken);
+  const setToken = useAuthToken(state => state.setAuthToken);
+  console.log("token:", token);
 
+  const [intro, setIntro] = useState<boolean | null>(null);
+  const [solved, setSolved] = useState(false);
+  const startApp = useCallback(async () => {
+    const token = await getToken();
+    const profile = await getProfile();
+    const intro = await AsyncStorage.getItem('intro');
+    console.log("token:", token, "profile:", profile, "intro:", intro);
+
+    Promise.all([token, profile, intro]).then(res => {
+      if (intro) {
+        setIntro(JSON.parse(intro));
+      } else {
+        setIntro(null);
+      }
+      if (token && profile) {
+        setToken(token);
+        setSolved(true);
+        RootNavigation?.onReady?.();
+        SplashScreen.hide();
+        initializeSocket(token);
+        try {
+          socket.on('connected', (response: any) => {});
+        } catch (error) {
+          console.error('An error occurred in the socket connection:', error);
+        }
+      } else {
+        RootNavigation?.onReady?.();
+        setSolved(true);
+        SplashScreen.hide();
+      }
+    });
+  }, [getToken, getProfile]);
+
+  useEffect(() => {
+    startApp()
+  }, []);
+
+console.log("solved:", solved);
   return (
-       <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Provider store={store}>
         <SafeAreaProvider>
-          <NavigationContainer>
+          <NavigationContainer  ref={RootNavigation.NavigationRef}>
             <StatusBar
               translucent
               backgroundColor="#fff"
               barStyle="dark-content"
             />
-            <StackNavigation token={token} intro={intro} />
+           {solved && <StackNavigation token={token} intro={intro} />}
           </NavigationContainer>
         </SafeAreaProvider>
       </Provider>
     </GestureHandlerRootView>
   );
-};
+}
 
 export default App;
--- android/app/src/main/java/com/hotspotmeet/MainActivity.kt
+++ android/app/src/main/java/com/hotspotmeet/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.hotspotmeet
 
+import android.os.Bundle; 
 import com.facebook.react.ReactActivity
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)   // 👈 show splash
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
